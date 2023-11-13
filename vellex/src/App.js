8906b97616dabae6a31fc0f76ec6d76577a0e12c
itHautier <scoozy23@gmail.com>
2023-11-13 19:39:26 +0100

    section page

--- vellex/src/App.js
+++ vellex/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 section-40vh">Première section</div>
+          <div className="col-12 section-60vh">Deuxième section </div>
+          <div className="col-12 section-60vh">Troisième section</div>
+          <div className="col-12 section-120vh">Quatrième section </div>
+          <div className="col-12 section-30vh">Cinquieme section </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ vellex/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <p>&copy; 2023 Votre Société. Tous droits réservés.</p>
+    </footer>
+  );
+}
+
+export default Footer;
