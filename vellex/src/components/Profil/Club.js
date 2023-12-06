d3c984bf989526c4626e93496d576f5c8bfa1e32
itHautier <scoozy23@gmail.com>
2023-12-06 18:15:32 +0100

    profil page

--- vellex/src/components/Profil/Club.js
+++ vellex/src/components/Profil/Club.js
@@ -1,13 +1,84 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ProfilePage.css';
 import Sidebar from './SideBar';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import clubImage from '../../assets/images/chevre.jpg';
 
 const Club = () => {
+  const clubCoordinates = {
+    latitude: 48.8566,
+    longitude: 2.3522,
+  };
+
+  useEffect(() => {
+    const mapContainer = L.DomUtil.get('map');
+
+    if (mapContainer != null) {
+      mapContainer._leaflet_id = null;
+    }
+
+    const clubIcon = new L.Icon({
+      iconUrl: clubImage,
+      iconSize: [50, 50],
+      iconAnchor: [25, 50],
+      popupAnchor: [0, -50],
+    });
+
+    const mapTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+    const map = L.map('map').setView([clubCoordinates.latitude, clubCoordinates.longitude], 15);
+
+    L.tileLayer(mapTiles, {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(map);
+
+    L.marker([clubCoordinates.latitude, clubCoordinates.longitude], { icon: clubIcon }).addTo(map);
+
+     return () => {
+      map.remove();
+    };
+  }, [clubCoordinates]);
+
   return (
     <div className="profile-container">
       <Sidebar />
-      <div className="content">
-        <h1>Mon club</h1>
+      <div className="content" style={{color:'#225B7C'}}>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+          <div style={{ textAlign: 'center' }}>
+            <img src={clubImage} alt="Club" style={{ borderRadius: '50%', width: '100px', height: '100px' }} />
+            <h2>Nom du Club</h2>
+          </div>
+          <button style={{color:'#CBFACF', backgroundColor:'#00997B', border:'none', borderRadius:'6px'}}>Créer événement</button>
+        </div>
+
+        <div style={{ display: 'flex', marginTop: '20px' }}>
+          <div style={{ flex: 1 }}>
+            <h3>Coordonnées du Club</h3>
+            <p>Adresse: 123 Rue du Club</p>
+            <p>Ville: Ville Club</p>
+            <p>Téléphone: 123-456-7890</p>
+          </div>
+
+          <div style={{ flex: 1 }}>
+            <h3>Localisation</h3>
+            <div
+              id="map"
+              style={{ width: '100%', height: '300px', borderRadius: '10px', overflow: 'hidden' }}
+            ></div>
+          </div>
+        </div>
+        <div style={{ marginTop: '20px' }}>
+          <h3>Description du Club</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Vivamus aliquet,
+            turpis et cursus aliquet, sem augue pharetra libero, in facilisis odio felis eu elit.
+          </p>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+          <button style={{color:'#CBFACF', backgroundColor:'#00997B', border:'none', borderRadius:'6px'}}>Mettre à jour</button>
+        </div>
       </div>
     </div>
   );
--- vellex/src/components/Profil/ProfilePage.js
+++ vellex/src/components/Profil/ProfilePage.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import './ProfilePage.css';
 import Sidebar from './SideBar';
+import profilImage from '../../assets/images/chevre.jpg';
 
 const ProfilePage = () => {
   return (
-    <div className="profile-container">
+    <div className="profile-container "  style={{color:'#225B7C'}}>
       <Sidebar />
-      <div className="content">
-        <h1>Mes coordonnées</h1>
+      
+      <div className='container'>
+      
+        <div className="centered">
+        <h2 className='title'>Mes coordonées</h2>
+          <img src={profilImage} alt="Avatar" className="profilImage" />
+        </div>
+        <div className="columnsContainer">
+          <div className="leftColumn">
+            <div className="fieldGroup">
+              <label>Nom:</label>
+              <input type="text" />
+            </div>
+            <div className="fieldGroup">
+              <label>Prénom:</label>
+              <input type="text" />
+            </div>
+            <div className="fieldGroup">
+              <label>Email:</label>
+              <input type="email" />
+            </div>
+          </div>
+
+          <div className="verticalSeparator"></div>
+
+          <div className="rightColumn">
+            <div className="fieldGroup">
+              <label>Mot de passe actuel:</label>
+              <input type="password" />
+            </div>
+            <div className="fieldGroup">
+              <label>Modifier mot de passe:</label>
+              <input type="password" />
+            </div>
+          </div>
+          </div>
       </div>
     </div>
   );
