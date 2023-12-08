e161b934e775bb01b2917d3cec52eacbaea88201
benachouranass <benachouranass@gmail.com>
2023-12-08 09:14:28 +0100

    minor-changes

--- vellex/src/components/LandingPage/actu/Actu.js
+++ vellex/src/components/LandingPage/actu/Actu.js
@@ -1,94 +1,97 @@
-import React from 'react';
-import './Actu.css';
+import React from "react";
+import "./Actu.css";
 
 const Actu = () => {
   return (
     <div className="section">
-        <div className='title-section'>Les dernieres actualités</div>
-        <div className='actu-section'>
-            <div className='container-un'>
-                <img src='/win.jpg' />
-                <div className='text-win'>
-                    <h2>Titre de l'évenement</h2>
-                    <p>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                    </p>
-                    <div className='container-btn'><button className='boot'>En savoir +</button></div>
-                </div>
+      <div className="title-section">Les dernieres actualités</div>
+      <div className="actu-section">
+        <div className="container-un">
+          <img src="/win.jpg" />
+          <div className="text-win">
+            <h2>Titre de l'évenement</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum Bla
+            </p>
+            <div className="container-btn">
+              <button className="boot">En savoir +</button>
             </div>
-            <div className='container-deux'>
-                <img src='/vtt.jpg'/>
-                <div className='text-win'>
-                    <h2>Titre de l'évenement</h2>
-                    <p>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                    </p>
-                    <div className='container-btn'><button className='boot'>En savoir +</button></div>
-                </div>
-            </div>
-            <div className='container-deux'>
-                <img src='/vtt.jpg'/>
-                <div className='text-win'>
-                    <h2>Titre de l'évenement</h2>
-                    <p>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                    </p>
-                    <div className='container-btn'><button className='boot'>En savoir +</button></div>
-                </div>
-            </div>
-            <div className='container-deux'>
-                <img src='/vtt.jpg'/>
-                <div className='text-win'>
-                    <h2>Titre de l'évenement</h2>
-                    <p>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                    </p>
-                    <div className='container-btn'><button className='boot'>En savoir +</button></div>
-                </div>
-            </div>
-            <div className='container-deux'>
-                <img src='/vtt.jpg'/>
-                <div className='text-win'>
-                    <h2>Titre de l'évenement</h2>
-                    <p>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                        Bla Bla Bla Bla Bla Bla Bla Bla Bla <br/>
-                    </p>
-                    <div className='container-btn'><button className='boot'>En savoir +</button></div>
-                </div>
-            </div>
+          </div>
         </div>
+        <div className="container-deux">
+          <img src="/vtt.jpg" />
+          <div className="text-win">
+            <h2>Titre de l'évenement</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nulla pariatur. <br />
+              Excepteur sint occaecat cupidatat non proident
+            </p>
+            <div className="container-btn">
+              <button className="boot">En savoir +</button>
+            </div>
+          </div>
+        </div>
+        <div className="container-deux">
+          <img src="/vtt.jpg" />
+          <div className="text-win">
+            <h2>Titre de l'évenement</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua,
+              Excepteur sint occaecat.
+              <br />
+              Excepteur sint occaecat cupidatat non proident, Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit.
+            </p>
+            <div className="container-btn">
+              <button className="boot">En savoir +</button>
+            </div>
+          </div>
+        </div>
+        <div className="container-deux">
+          <img src="/vtt.jpg" />
+          <div className="text-win">
+            <h2>Titre de l'évenement</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nulla pariatur. <br />
+              Excepteur sint occaecat cupidatat non proident
+            </p>
+            <div className="container-btn">
+              <button className="boot">En savoir +</button>
+            </div>
+          </div>
+        </div>
+        <div className="container-deux">
+          <img src="/vtt.jpg" />
+          <div className="text-win">
+            <h2>Titre de l'évenement</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nulla pariatur. <br />
+              Excepteur sint occaecat cupidatat non proident
+            </p>
+            <div className="container-btn">
+              <button className="boot">En savoir +</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Actu;
--- vellex/src/components/Profil/Past.js
+++ vellex/src/components/Profil/Past.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import './ProfilePage.css';
-import Sidebar from './SideBar';
+import React from "react";
+import "./ProfilePage.css";
+import Sidebar from "./SideBar";
 import Event from "../Events/Event";
-import Header from '../Header';
+import Header from "../Header";
 const Past = () => {
   return (
     <>
-    <Header />
-    <div className="profile-container">
-      <Sidebar />
-      <div>
-            <div className="content">
-                <h1>Mes événements passé</h1>
-            </div>
-            <div className='futurEvent'>
-                <Event />
-                <Event />
-            </div>
+      <Header />
+      <div className="profile-container">
+        <Sidebar />
+        <div>
+          <div className="content">
+            <h1>Mes événements passé</h1>
+          </div>
+          <div className="futurEvent"></div>
         </div>
-    </div>
+      </div>
     </>
   );
 };
